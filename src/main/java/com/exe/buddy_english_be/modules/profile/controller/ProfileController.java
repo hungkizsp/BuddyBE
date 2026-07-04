e9c7f9d6009178c4cd47b9216c5a9e0c212d3358
Sadlongvip <sadlongvip@gmail.com>
2026-07-04 09:08:41 +0700

    update(controller): add name behide @Restcontroller

--- src/main/java/com/exe/buddy_english_be/modules/profile/controller/ProfileController.java
+++ src/main/java/com/exe/buddy_english_be/modules/profile/controller/ProfileController.java
@@ -22,11 +22,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Validated
+@RestController("modulesProfileController")
 @RequestMapping("/api/profile")
 @AllArgsConstructor
 public class ProfileController {
--- src/main/java/com/exe/buddy_english_be/modules/user/controller/UserController.java
+++ src/main/java/com/exe/buddy_english_be/modules/user/controller/UserController.java
@@ -19,11 +19,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Validated
+@RestController("modulesUserController")
 @RequestMapping("/api/users")
 @AllArgsConstructor
 public class UserController {
